7870ed2e3c71f569508ecb9245457360954da109
Darell Jimenez <jabrielthj07@gmail.com>
2026-08-07 14:41:32 -0600

    chore(project): configurando tema y rutas de la aplicacion

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+
+  static const String splash = '/';
+  static const String welcome = '/welcome';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+
+}
