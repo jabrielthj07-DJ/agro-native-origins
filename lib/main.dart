6dcf551f877aab3bcbc59f4908eebe9de0bacd46
Darell Jimenez <jabrielthj07@gmail.com>
2026-08-28 23:52:43 -0600

    fix(routes): correccion en configuracion de rutas y navegacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,37 @@
-// Leyenda
-// PNC = Pantalla de carga
-// W = Welcome
-// LG = Login
-//Rg = Registrarse
-import 'screens/auth/register/register_screen.dart'; // Rg
-import 'screens/auth/login/login_screen.dart'; // LG
-import 'screens/welcome/welcome_screen.dart'; // W
-import 'screens/splash/splash_screen.dart'; // PNC
-
 import 'package:flutter/material.dart';
 
-import 'core/theme/apptheme.dart';
 import 'core/routes/app_routes.dart';
+import 'core/theme/apptheme.dart';
 
+import 'screens/splash/splash_screen.dart';
+import 'screens/welcome/welcome_screen.dart';
+import 'screens/auth/login/login_screen.dart';
+import 'screens/auth/register/register_screen.dart';
+import 'screens/home/home_screen.dart';
 
 void main() {
   runApp(const AgroNativeOriginsApp());
 }
 
-
 class AgroNativeOriginsApp extends StatelessWidget {
-
   const AgroNativeOriginsApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
-
       title: 'Agro Native Origins',
-
       theme: AppTheme.lightTheme,
 
       initialRoute: AppRoutes.splash,
 
-
       routes: {
-
-     AppRoutes.splash: (context) => const SplashScreen(), //PNC
-
-     AppRoutes.welcome: (context) => const WelcomeScreen(), // W
-
-     AppRoutes.login: (context) => const LoginScreen(), // LG
-
-     AppRoutes.register: (context) => const RegisterScreen(),// Rg
-
+        AppRoutes.splash: (context) => const SplashScreen(),
+        AppRoutes.welcome: (context) => const WelcomeScreen(),
+        AppRoutes.login: (context) => const LoginScreen(),
+        AppRoutes.register: (context) => const RegisterScreen(),
+        AppRoutes.home: (context) => const HomeScreen(),
       },
-
     );
-
   }
-
 }
